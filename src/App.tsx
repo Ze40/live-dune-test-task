4e206ea3bce136cf3cfd060bbbf87cfce529f935
Ze40 <maxim.schcerbakov@gmail.com>
2025-07-02 12:54:30 +0300

    chore(routing): provide

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter } from "react-router";
+
+import { AuthHeader } from "./widgets";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <AuthHeader type="login" />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
+import "./style/base.scss";
+import "./style/main.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
