33483399d882fba748f8184ffb11383718cb2c5f
Ze40 <maxim.schcerbakov@gmail.com>
2025-07-03 18:31:51 +0300

    feat(route): create base routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
-import { AuthHeader } from "./widgets";
+import { AuthLayout } from "./pages/auth/AuthLayout";
+import LoginPage from "./pages/auth/login/LoginPage";
+import RegisterPage from "./pages/auth/register/RegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <AuthHeader type="login" />
+      <Routes>
+        <Route index element={<Navigate to={"auth"} replace />} />
+        <Route path="auth" element={<AuthLayout />}>
+          <Route index element={<Navigate to="login" replace />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
