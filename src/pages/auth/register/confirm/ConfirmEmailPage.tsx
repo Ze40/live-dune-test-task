92719a998e1e5301876ca2b088462cef39951bc1
Ze40 <maxim.schcerbakov@gmail.com>
2025-07-11 18:32:18 +0300

    feat(pages): create confirm page

--- src/pages/auth/register/confirm/ConfirmEmailPage.tsx
+++ src/pages/auth/register/confirm/ConfirmEmailPage.tsx
@@ -1,5 +1,25 @@
+import clsx from "clsx";
+
+import { Button } from "@/shared/ui";
+
+import classes from "./style.module.scss";
+
 const ConfirmEmailPage = () => {
-  return <main></main>;
+  return (
+    <div className={classes.container}>
+      <h2 className="large-header-text">Подтвердите ваш e-mail</h2>
+      <p className={clsx("small-sub-text", classes.subtitle)}>
+        {"Игорь"}, на ваш E-mail отправлено письмо со ссылкой для подтверждения. Перейдите по ней,
+        чтобы активировать вашу учетную запись и получить 7 дней бесплатного доступа.
+      </p>
+      <Button variant="fill" className={classes.btn} link="mailto:">
+        Перейти к почте
+      </Button>
+      <Button variant={"empty"} link="/auth/register/confirm/didnt-get-email">
+        Мне не пришло письмо
+      </Button>
+    </div>
+  );
 };
 
 export default ConfirmEmailPage;
